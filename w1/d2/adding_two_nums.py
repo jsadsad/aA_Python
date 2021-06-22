b5a812c93987b2e9c1565895fb24fa0f91be071c
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-22 14:20:39 -0700

    adding two_nums

---
+++ w1/d2/adding_two_nums.py
@@ -0,0 +1,8 @@
+# Define your function "addition" here
+def addition(num_1, num_2):
+    return num_1 + num_2
+
+
+print(addition(2, 3))  # > 5
+print(addition(-3, -6))  # > -9
+print(addition(7, 3))  # > 10
