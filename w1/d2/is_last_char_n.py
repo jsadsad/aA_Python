ed87d09e2f7618425a771c1b81d0f66fb5069a9a
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-22 19:01:18 -0700

    added is last char n

---
+++ w1/d2/is_last_char_n.py
@@ -0,0 +1,9 @@
+# Write your function, here.
+def is_last_character_n(string):
+    return string[-1] == "n"
+
+
+print(is_last_character_n("Aiden"))  # > True
+print(is_last_character_n("Piet"))  # > False
+print(is_last_character_n("Bert"))  # > False
+print(is_last_character_n("Dean"))  # > True
