97de6a098f81231ecf9a1769f61c55a5e4d7eade
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-22 17:33:29 -0700

    remainder py

---
+++ w1/d2/remainder.py
@@ -0,0 +1,11 @@
+# Write your function, here.
+def remainder(num_1, num_2):
+    return num_1 % num_2
+
+
+print(remainder(1, 3))  # > 1
+print(remainder(3, 4))  # > 3
+print(remainder(5, 5))  # > 0
+print(remainder(7, 2))  # > 1
+print(remainder(26, 10))  # > 6
+print(remainder(26, 40))  # > 26
