dc6146dbf8012837a5e270910b5e126193d29476
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-22 19:29:48 -0700

    tupple comprehension

--- w1/d3/lists_in_python.py
+++ w1/d3/lists_in_python.py
@@ -28,6 +28,22 @@ to_filter_list = [1, 10, 11, 12, 30, 100, 90]
 
 filtered_list = [n for n in to_filter_list if n % 3 == 0]
 
-print(filtered_list)
+# print(filtered_list)
 
 # it is second nature to write in list comprehension for python
+
+# creating a tupple
+
+letters = ["a", "b", "c"]
+nums = [1, 2]
+
+tupple_list = []
+for l in letters:
+    for n in nums:
+        tupple_list.append((n, l))
+
+## creating a tupple with list comprehension
+tupple_list_comprehension = [(n, l) for n in nums for l in letters]
+
+print(tupple_list_comprehension)
+# print(tupple_list)
