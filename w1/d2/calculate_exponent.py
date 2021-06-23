3b91c84223effff81609f2a7eaba5006476fdeac
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-22 17:35:04 -0700

    added calculate exponent

---
+++ w1/d2/calculate_exponent.py
@@ -0,0 +1,8 @@
+# Write your function, here.
+def calculate_exponent(num_1, num_2):
+    return num_1 ** num_2
+
+
+print(calculate_exponent(5, 5))  # > 3125
+print(calculate_exponent(10, 10))  # > 10000000000
+print(calculate_exponent(3, 3))  # > 27
