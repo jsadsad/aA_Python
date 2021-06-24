a2d7454ea28e9b2a72c0b29bb9985777f714e586
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-24 16:56:45 -0700

    range loops

---
+++ w1/d3/range_loops.py
@@ -0,0 +1,6 @@
+# Powers of 2 from 1 to 16
+# Write a for loop that uses the range function to
+# print the powers of 2 from 2 - 65536, that is
+# from 2^1st - 2^16th powers
+for i in range(1, 17):
+    print(2 ** i)
