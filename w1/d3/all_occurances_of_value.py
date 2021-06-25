63e631f1b7650a98a0eb0cbec41e54b80bafbd8b
Joshua C. Sadsad <joshsad2@gmail.com>
2021-06-24 17:03:02 -0700

    all occurances of value

---
+++ w1/d3/all_occurances_of_value.py
@@ -0,0 +1,20 @@
+# Write your function, here.
+def get_indices(lst, val):
+  indices = []
+  for i in range(0, len(lst)):
+    if val == lst[i]:
+      indices.append(i)
+  return indices
+
+
+print(get_indices(["a", "a", "b", "a", "b", "a"], "a"))
+# Prints [0, 1, 3, 5]
+
+print(get_indices([1, 5, 5, 2, 7], 7))
+# Prints [4]
+
+print(get_indices([1, 5, 5, 2, 7], 5))
+# Prints [1, 2]
+
+print(get_indices([1, 5, 5, 2, 7], 8))
+# Prints []
